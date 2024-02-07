18a6c4363cfd51bb18bbe7647383e8507debee07
djamelhemch <mohameddjamel.hemch.dz@viacesi.fr>
2024-02-07 14:36:52 +0100

    updated printer script to handle default printer and close adobe once print is done

--- printer.py
+++ printer.py
@@ -1,5 +1,6 @@
 import subprocess
 import time
+import win32print
 
 class PDFPrinter:
     def __init__(self, acrobat_path):
@@ -16,24 +17,33 @@ class PDFPrinter:
             print("Printing successful")
         else:
             print("Printing failed. Error:", stderr.decode('utf-8'))
+    def kill_print_process(self):
+        if self.print_process and self.print_process.poll() is None:
+            print("Killing print process")
+            self.print_process.terminate()
+            time.sleep(1)  # Allow time for termination
+            if self.print_process.poll() is None:
+                self.print_process.kill()
+
+def get_default_printer():
+    default_printer_info = win32print.GetDefaultPrinter()
+    default_printer_name = default_printer_info.split(',')[0]
+    return default_printer_name
 
 def print_pdf(pdf_content, output_file="output.pdf"):
     with open(output_file, "wb") as pdf_file:
         pdf_file.write(pdf_content)
 
-    # Set the path to your PDF viewer executable (e.g., Adobe Acrobat Reader)
-    pdf_viewer_path = r'C:\Program Files\Adobe\Acrobat DC\Acrobat\Acrobat.exe'
-
-    # Open the PDF file with the default PDF viewer
-    subprocess.run([pdf_viewer_path, output_file], shell=True)
-
-    # Introduce a delay before starting the printing process
-    time.sleep(1)  # Adjust the delay time as needed
-
+    default_printer = get_default_printer()
     # Start the printing process with Adobe Acrobat
     acrobat_path = r'C:\Program Files (x86)\Adobe\Acrobat 11.0\Acrobat\Acrobat.exe'  # Adjust the path as needed
     pdf_printer = PDFPrinter(acrobat_path)
-    pdf_printer.print_pdf_with_acrobat(output_file, 'Your_Printer_Name')  # Replace 'Your_Printer_Name' with your printer name
+    pdf_printer.print_pdf_with_acrobat(output_file, default_printer)  # Replace with your printer name
+    # Allow time for printing
+    time.sleep(10)
+
+    # Kill the print process
+    pdf_printer.kill_print_process()
 
 if __name__ == "__main__":
     # For testing the print_pdf function independently
